e1bc7d9e125aa9dc0bae8752b19c769bb0234f69
Michael Hazboun <hazbo002@umn.edu>
2024-05-01 22:24:13 -0500

    Added comments

--- shiny/shiny_final/app.R
+++ shiny/shiny_final/app.R
@@ -1,35 +1,34 @@
-setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
-library(shiny)
-library(ggplot2)
+library(shiny) #required package
+library(ggplot2) #needed it for visualization, didn't use tidyverse to minimize run time
 
-import_tbl <- readRDS("import.RDS")
+import_tbl <- readRDS("import.RDS") #import data that will be used
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
-    titlePanel("Final Shiny Project"),
+    titlePanel("Three potential predictors of income"), #set title
 
     # Sidebar 
     sidebarLayout(
         sidebarPanel(
-          sliderInput("sizepoint",
-                      label = "Change the size of the points",
-                      min = 1,
-                      max=10,
-                      value=5
+          sliderInput("sizepoint", #added a slider than can be used to change the size. This was the only thing on a continuous scale so I choose to use a slider
+                      label = "Change the size of the points", #what we see
+                      min = 1, #minimum value possible
+                      max=10,  #maximum value possible
+                      value=5  #default value
           ),
-          radioButtons("varselect",
-                       label = "Which variable would you like to see?",
-                       choices= c("Is Astrology Science?"="ASTROSCI",
+          radioButtons("varselect", #radio buttons were just convenient and functional for my goals, so I used them for everything from this point on.
+                       label = "Which variable would you like to see?", #what we see
+                       choices= c("Is Astrology Science?"="ASTROSCI", #options, also assigned the options to their code in the data set for easier use later
                                   "Money compared to parents?"="PARSOL",
                                   "Number of pets?"="NUMPETS"),
-                       selected = "ASTROSCI"),
-          radioButtons("error",
-                       label= "Do you want to see error bars?",
+                       selected = "ASTROSCI"), #default value
+          radioButtons("error", 
+                       label= "Do you want to see error bars?", #practically same comments, no need to repeat 
                        choices=c("Yes"=TRUE,
                                  "No"=FALSE),
                        selected = TRUE),
-          radioButtons("fifteen_k", #could change this to do I want the line to be lm or something else...
+          radioButtons("fifteen_k", 
                        label = "Do you want to see the people that made less than 15k?",
                        choices=c("Yes","No"),
                        selected = "Yes"),
@@ -57,23 +56,20 @@ ui <- fluidPage(
 server <- function(input, output) {
 
     output$distPlot <- renderPlot({
-    filtered_tbl <- import_tbl
-      if(input$fifteen_k == "No")  
+      if(input$fifteen_k == "No")  #did an if statement to define the desired dataset 
       filtered_tbl <- filter(import_tbl, Income_10==TRUE) else filtered_tbl <- import_tbl
-    if(input$varselect == "ASTROSCI")
+    if(input$varselect == "ASTROSCI") #did an if statement to define the y values used based on the selected options. If was just simple, logical and easy, the first thing that came to mind on how I could do what I wanted, quickly.
       y_val <- filtered_tbl$ASTROSCI 
     else if(input$varselect == "PARSOL")
       y_val <-filtered_tbl$PARSOL 
     else y_val <-filtered_tbl$NUMPETS
- 
-    size_thing <- input$sizepoint
       
         #same comments will not be restated
-        ggplot(filtered_tbl,aes(x=INCOME,y=y_val))+ 
-        geom_jitter(width=0.3,height = 0.3,size=input$sizepoint,color=input$colorpoint)+ 
-        geom_smooth(method="lm",
-                    se=as.logical(input$error),
-                    color=input$colorline)
+        ggplot(filtered_tbl,aes(x=INCOME,y=y_val))+ #y is y_val to have it change with the options chosen
+        geom_jitter(width=0.3,height = 0.3,size=input$sizepoint,color=input$colorpoint)+ # allows people to change the shape and size of the points from the given options
+        geom_smooth(method="lm", # the smaller sample size when I changed things to only 15K income and more was causing problems with the default method, so I switched to lm
+                    se=as.logical(input$error), #had to add as.logical because the value was moving as character, this lets people remove or show the error bands as desired
+                    color=input$colorline) # this allows them to change the color of the line to one of the three colors given
 
             })
 }
